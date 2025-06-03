5c936e225d3c9cac0a24f566d2f9eb0f1573f144
Angel Garza <agarza@aph.mx>
2025-06-03 17:40:43 -0600

    Cambio el modificador de acceso internal por publico en los ViewModels

--- zaraga.weather/Pages/About/AboutViewModel.cs
+++ zaraga.weather/Pages/About/AboutViewModel.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Maui.ApplicationModel;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,34 +8,4 @@ namespace zaraga.weather.Pages.About;
 
 public class AboutViewModel : SharedViewModel
 {
-
-    public Command OpenBrowserCommand => new Command(OpenBrowser);
-    public Command OpenBassDevCommand => new Command(OpenBassDev);
-
-
-    /// <summary>
-    /// Abre el navegador web con la pagina web de WeatherAPI
-    /// </summary>
-    private async void OpenBrowser()
-    {
-        await Browser.Default.OpenAsync(new Uri("https://www.weatherapi.com"), new BrowserLaunchOptions
-        {
-            LaunchMode = BrowserLaunchMode.SystemPreferred,
-            TitleMode = BrowserTitleMode.Show,
-            PreferredToolbarColor = Color.FromArgb("#2B0B98"),
-            PreferredControlColor = Colors.SandyBrown
-        });
-    }
-
-    private async void OpenBassDev()
-    {
-        await Browser.Default.OpenAsync(new Uri("https://bas.dev/work/meteocons"), new BrowserLaunchOptions
-        {
-            LaunchMode = BrowserLaunchMode.SystemPreferred,
-            TitleMode = BrowserTitleMode.Show,
-            PreferredToolbarColor = Color.FromArgb("#2B0B98"),
-            PreferredControlColor = Colors.SandyBrown
-        });
-    }
-
 }
--- zaraga.weather/Pages/Settings/SettingsViewModel.cs
+++ zaraga.weather/Pages/Settings/SettingsViewModel.cs
@@ -1,10 +1,6 @@
-﻿using Java.Security;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Storage;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,46 +8,4 @@ namespace zaraga.weather.Pages.Settings;
 
 public class SettingsViewModel : SharedViewModel
 {
-    private int _selectedIconStyle;
-
-
-    public int SelectedIConStyle { get => _selectedIconStyle; set { _selectedIconStyle = value; OnIconStyleChange(value); } }
-
-
-    public Command LoadSettingsCommand => new Command(LoadSettings);
-    public Command GoBackCommand => new Command(GoBack);
-
-
-    public SettingsViewModel()
-    {
-        LoadSettings();
-    }
-
-    private void LoadSettings()
-    {
-        try
-        {
-
-            _selectedIconStyle = Preferences.Default.Get("SelectedIconStyle", 0);
-        }
-        catch (Exception)
-        {
-            Preferences.Default.Clear("SelectedIconStyle");
-            _selectedIconStyle = 0;
-        }
-    }
-
-    private void OnIconStyleChange(int selectedIconStyle)
-    {
-        OnPropertyChanged(nameof(SelectedIConStyle));
-        Preferences.Default.Set("SelectedIconStyle", selectedIconStyle);
-    }
-
-    private void GoBack()
-    {
-
-        //Shell.Current.GoToAsync("..");
-        Shell.Current.GoToAsync("//HomePage");
-    }
-
 }
