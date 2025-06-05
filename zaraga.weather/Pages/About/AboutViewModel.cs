3fa88a97a38663c17c3596a26599b486760795eb
Angel Garza <agarza@aph.mx>
2025-06-04 19:28:46 -0600

    Carga de imagenes animada, fijas y con bordes para los pronosticos de clima

--- zaraga.weather/Pages/About/AboutViewModel.cs
+++ zaraga.weather/Pages/About/AboutViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +11,22 @@ namespace zaraga.weather.Pages.About;
 
 public class AboutViewModel : SharedViewModel
 {
+
+    public Command OpenBrowserCommand => new Command(OpenBrowser);
+
+
+    /// <summary>
+    /// Abre el navegador web con la pagina web de WeatherAPI
+    /// </summary>
+    private async void OpenBrowser()
+    {
+        await Browser.Default.OpenAsync(new Uri("https://www.weatherapi.com"), new BrowserLaunchOptions
+        {
+            LaunchMode = BrowserLaunchMode.SystemPreferred,
+            TitleMode = BrowserTitleMode.Show,
+            PreferredToolbarColor = Color.FromArgb("#2B0B98"),
+            PreferredControlColor = Colors.SandyBrown
+        });
+    }
+
 }
