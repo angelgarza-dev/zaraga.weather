62d63a0b1c3b6ccad35f2a1f6c0fd4620019f921
Angel Garza <agarza@aph.mx>
2025-06-09 15:06:57 -0600

    Diseño en pantalla de iconos

--- zaraga.weather/Pages/Settings/SettingsViewModel.cs
+++ zaraga.weather/Pages/Settings/SettingsViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Java.Security;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,4 +12,41 @@ namespace zaraga.weather.Pages.Settings;
 
 public class SettingsViewModel : SharedViewModel
 {
+    private int _selectedIconStyle;
+
+
+    public int SelectedIConStyle { get => _selectedIconStyle; set { _selectedIconStyle = value; OnIconStyleChange(value); } }
+
+
+    public Command LoadSettingsCommand => new Command(LoadSettings);
+
+    public SettingsViewModel()
+    {
+        LoadSettings();
+    }
+
+    private void LoadSettings()
+    {
+        try
+        {
+
+            _selectedIconStyle = Preferences.Default.Get("SelectedIconStyle", 0);
+        }
+        catch (Exception)
+        {
+            Preferences.Default.Clear("SelectedIconStyle");
+            _selectedIconStyle = 0;
+        }
+    }
+
+
+
+
+    private void OnIconStyleChange(int selectedIconStyle)
+    {
+        OnPropertyChanged(nameof(SelectedIConStyle));
+        Preferences.Default.Set("SelectedIconStyle", selectedIconStyle);
+    }
+
+
 }
