d7ce4e5648667c83e4e399d3cdeecb2f42432c3d
Angel Gustavo Garza Aguilar <angel_winy@live.com.mx>
2025-06-23 19:48:46 -0600

    Funcionalidad de consulta de pronostico de dias posteriores

--- zaraga.weather/Platforms/Android/MainActivity.cs
+++ zaraga.weather/Platforms/Android/MainActivity.cs
@@ -8,6 +8,7 @@ namespace zaraga.weather;
 [Activity(Theme = "@style/Maui.SplashTheme",
     MainLauncher = true,
     LaunchMode = LaunchMode.SingleTop,
+    ScreenOrientation = ScreenOrientation.Portrait,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
