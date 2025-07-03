73c79f1e98e8ff0406b2726b9d0d862daff5f9b9
Angel Gustavo Garza Aguilar <angel_winy@live.com.mx>
2025-07-03 14:33:48 -0600

    ajuste en el diseño de los radio buttons

--- zaraga.weather/Converters/IconCodeConverter.cs
+++ zaraga.weather/Converters/IconCodeConverter.cs
@@ -1,28 +1,23 @@
 ﻿using Microsoft.Maui.Controls;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using zaraga.weather.Services;
 
-namespace zaraga.weather.Converters
-{
-    internal class IconCodeConverter : IValueConverter
-    {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            if (int.TryParse(value?.ToString(), out int code) == true)
-            {
-                return WeatherCodes.GetWeatherImageByCode(code);
-            }
-            return App.NotAvailableIcon; // or a default image if you prefer
-        }
+namespace zaraga.weather.Converters;
 
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+internal class IconCodeConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (int.TryParse(value?.ToString(), out int code) == true)
         {
-            return App.NotAvailableIcon;
+            return WeatherCodes.GetWeatherImageByCode(code);
         }
+        return App.NotAvailableIcon; // or a default image if you prefer
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return App.NotAvailableIcon;
     }
 }
